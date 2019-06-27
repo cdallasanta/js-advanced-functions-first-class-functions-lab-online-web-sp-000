e4662cf308fa6c220bb0d90d0fe81e356e9a128f
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-27 22:12:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const returnFirstTwoDrivers = function(arr){
   return arr.slice(0, 2);
 };
 
-const returnLastTwoDrivers(arr) {
+const returnLastTwoDrivers = function(arr) {
   return arr.slice(-2);
 };
 
